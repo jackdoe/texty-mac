69e3ec3e8978221b8ea1d303241c3360c83879dd
borislav nikolov <vf1100c@gmail.com>
2011-12-16 12:18:54 +0200

    oops, forgot /

--- texty/Preferences.h
+++ texty/Preferences.h
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-#define TEXTY_DIR @"~TEXTY_DATA"
+#define TEXTY_DIR @"~/TEXTY_DATA"
 #define DEFAULT_COMMAND @"http://localhost:3000/"
 #define AUTOSAVE_INTERVAL @"60" 
 #define TERMINATE_ON_CLOSE @"YES"
